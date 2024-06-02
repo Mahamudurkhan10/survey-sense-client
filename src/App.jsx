7b0b096da4d2f235b6ea85cd57526a8f259fa794
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-06-02 13:25:11 +0600

    clinet side add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className=' mb-5 text-5xl text-green-400'>Vite + React</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
