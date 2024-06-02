c59ce7c3f1ed2e70c2ee311382d7225e72cc1805
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-06-02 18:32:01 +0600

    banner navbar done

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,55 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+const Banner = () => {
+     return (
+          <div>
+               <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    loop={true}
+                    pagination={{
+                         clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+               >
+                    <SwiperSlide >
+                         <img className=' h-[600px] relative opacity-100 w-full' src="https://i.ibb.co/1KFWWwb/organic-flat-feedback-concept-23-2148959061.jpg" alt="" />
+                         <div className='text-center absolute -mt-[400px] '>
+                              <h1 className='text-4xl w-3/4 mx-auto -mr-[100px] font-extrabold text-blue-700'>Tell Us What You Think - Take Our Survey Now </h1>
+                              <button className='btn btn-outline -mr-[400px] border-b-4 btn-success text-white text-2xl font-semibold mt-5 '> Join Us  </button>
+                         </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                         <img className=' h-[600px] relative opacity-100 w-full' src="https://i.ibb.co/rQDjLHh/tiny-business-people-using-rating-site-vote-people-computer-screen-rating-site-professional-rank-sit.jpg" alt="" />
+                         <div className='text-center absolute -mt-[400px] '>
+                              <h1 className='text-4xl w-3/4 mx-auto -mr-[100px] font-extrabold text-blue-700'>Join the Conversation - Let Us Know How We're Doing </h1>
+                              <button className='btn  -mr-[400px] border-b-4 btn-error text-white text-2xl font-semibold mt-5 '> Join Us  </button>
+                         </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                         <img className=' h-[600px] relative opacity-100 w-full' src="https://i.ibb.co/1ZtqR9R/3d-illustration-hand-putting-tick-paper.jpg" alt="" />
+                         <div className=' ml-[100px]  w-3/5 mx-auto absolute -mt-[400px] '>
+                              <h1 className='text-4xl  font-extrabold -mr-[100px] text-green-600'>Be Heard - Participate in Our Community  Feedback Survey </h1>
+                              <button className='btn btn-outline  border-b-4 btn-info text-white text-2xl font-semibold mt-5 '> Join Us  </button>
+                         </div>
+                    </SwiperSlide>
+
+                    {/* <SwiperSlide >
+                         <img className='h-[600px] opacity-100 w-full' src="https://i.ibb.co/rQDjLHh/tiny-business-people-using-rating-site-vote-people-computer-screen-rating-site-professional-rank-sit.jpg" alt="" />
+                       
+                    </SwiperSlide> */}
+
+
+
+               </Swiper>
+          </div>
+     );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+     const user = true;
+     const navItem = <>
+       <li> <NavLink to={'/'}><a> Home</a></NavLink> </li>
+       <li> <NavLink to={'/survey'}><a> Survey </a></NavLink> </li>
+       <li> <NavLink to={'/pricing'}><a> Pricing </a></NavLink> </li>
+     </>
+     const navItem2 = <>
+       
+       <li> <NavLink className={'btn btn-ghost'}  to={'/dashboard'}><a> Dashboard </a></NavLink> </li>
+       <li> <button className="btn btn-ghost">  <a> Logout </a></button> </li>
+     </>
+     return (
+          <div>
+               <div className="navbar bg-base-100">
+                    <div className="flex-1">
+                         <a className="btn btn-ghost text-xl"> Survey Sense </a>
+                    </div>
+                    <div className="flex-none">
+                         <div className="dropdown dropdown-end">
+                           <ul className="flex gap-12 mr-6">  {navItem}</ul>
+                             
+                         </div>
+                         <div className="dropdown dropdown-end">
+                              {
+                                   user? <div><div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                   <div className="w-10 rounded-full">
+                                        <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                   </div>
+                              </div>
+                              <ul tabIndex={0} className="menu space-y-3  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                   
+                                       {navItem2}
+                              </ul> </div>
+                              : <button className="btn btn-success text-white font-bold"> Join Us </button>
+
+                              }
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default Navbar;
